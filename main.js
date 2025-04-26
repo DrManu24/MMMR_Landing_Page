3d6ce52c3a11dd28bdbc35c2657a51165e7fa96d
Dhanush Kumar <dhanush64b@gmail.com>
2025-04-26 22:25:48 +0530

    made the following changes Merriweather for headings. Open sans for text body. Hamburger menu must collapse on clicking. Solid logo on banner and tab.

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 function toggleNav() {
     let nav = document.getElementById("nav-links");
     if (nav.style.height === "0px" || nav.style.height === "") {
-        nav.style.height = "218px";
+        nav.style.height = nav.scrollHeight + "px";
     } else {
         nav.style.height = "0px";
     }
 }
+document.querySelectorAll('#nav-links a').forEach(function(link) {
+    link.addEventListener('click', function() {
+      let nav = document.getElementById('nav-links');
+      if (nav.style.height !== "" && nav.style.height !== "auto") {
+        nav.style.height = '0px';
+      }
+    });
+  });
+  
+
